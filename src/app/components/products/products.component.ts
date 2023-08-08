89717b3f9baa4d2f94c135802b9a2d990f5e5d39
abdullah.shahid <abdullah.shahid@rolustech.net>
2023-08-08 14:31:07 +0500

    added product page

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Product } from '../../shared/interfaces/product';
+import { products } from '../../shared/models/products.model';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
+  filteredProducts: Product[] = [];
+  category: string = '';
 
-  constructor() { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.category = this.extractCategoryFromRoute();
+    this.populateProducts();
   }
 
+  private extractCategoryFromRoute(): string {
+    const urlSegments = this.activatedRoute.snapshot.url;
+    if (urlSegments.length > 0) {
+      // Assuming the category is the first segment in the URL
+      return urlSegments[0].path;
+    }
+    return '';
+  }
+
+  private populateProducts() {
+    this.products = products;
+    console.log(this.category);
+    this.filteredProducts =
+      this.category && this.category != 'home'
+        ? this.products.filter((p) => p.category === this.category)
+        : this.products;
+  }
 }
